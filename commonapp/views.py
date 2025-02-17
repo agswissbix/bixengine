d588db8de26ef97b312e9fbf5c8343c3f9c42cd6
agswissbix <108857221+agswissbix@users.noreply.github.com>
2025-02-17 16:11:33 +0100

    .

--- commonapp/views.py
+++ commonapp/views.py
@@ -129,6 +129,7 @@ def check_csrf(request):
 
 
 @ensure_csrf_cookie
+@csrf_exempt
 def csrf_test_view(request):
     print("Function: csrf_test_view")
     csrf_header = request.META.get('HTTP_X_CSRFTOKEN')
