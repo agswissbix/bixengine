cde287257f5944b41990b1a951234798bf894093
agswissbix <108857221+agswissbix@users.noreply.github.com>
2025-05-05 12:53:48 +0200

    .

--- customapp_belotti/urls.py
+++ customapp_belotti/urls.py
@@ -4,6 +4,8 @@ from customapp_belotti.views import *
 
 urlpatterns = [
     path('sync_utenti_adiutobixdata/', sync_utenti_adiutobixdata, name='sync_utenti_adiutobixdata'),
+    path('sync_formularigruppo_adiutobixdata/', sync_formularigruppo_adiutobixdata, name='sync_formularigruppo_adiutobixdata'),
+    path('sync_prodotti_adiutobixdata/', sync_prodotti_adiutobixdata, name='sync_prodotti_adiutobixdata'),
    
     
 ]
