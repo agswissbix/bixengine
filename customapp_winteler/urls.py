6d494f489a8b17a2307a8083d0ec68ab522d2b26
agswissbix <108857221+agswissbix@users.noreply.github.com>
2025-07-02 10:27:59 +0200

    .

--- customapp_winteler/urls.py
+++ customapp_winteler/urls.py
@@ -8,6 +8,7 @@ from customapp_winteler.views import *
 urlpatterns = [
     path('winteler_wip_barcode_scan/', winteler_wip_barcode_scan, name='winteler_wip_barcode_scan'),
     path('sync_wipbarcode_bixdata_adiuto/', sync_wipbarcode_bixdata_adiuto, name='sync_wipbarcode_bixdata_adiuto'),
+    path('script_update_wip_status/', script_update_wip_status, name='script_update_wip_status'),
     
 
     
