c7befef18034b5df9c0b13186c246cf636f6aa93
Mark-Assi <110045452+marcogarganigo@users.noreply.github.com>
2025-07-28 08:16:42 +0200

    Add initial scripts for Belotti and PitService apps
    Added script.py files for customapp_belotti and customapp_pitservice with basic function definitions and necessary imports. Also added an empty __init__.py file to initialize the package.

---
+++ customapp_belotti/script.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+from django_q.models import Schedule, Task
+from django.db import connection
+
+#DA LASCIARE SE NO DA TANTI ERRORI!
+
+def belotti():
+    print("belotti")
