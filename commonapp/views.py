29cffaf0d42f50eb5903deb81ac24f00673cf2eb
agswissbix <108857221+agswissbix@users.noreply.github.com>
2025-09-12 11:58:32 +0200

    .

--- commonapp/views.py
+++ commonapp/views.py
@@ -1958,6 +1958,17 @@ def save_record_fields(request):
         oremensili_record.values['ruolo'] = ruolo
         oremensili_record.save()
 
+    # ---ORE MENSILI---
+    if tableid == 'assenze':
+        assenza_record = UserRecord('assenze', recordid)
+        ore_assenza= Helper.safe_float(assenza_record.values['ore'])
+        dipendente_recordid = assenza_record.values['recordiddipendente_']
+        dipendente_record = UserRecord('dipendente', dipendente_recordid)
+        saldovacanze_attuale=Helper.safe_float(dipendente_record.values['saldovacanze'])
+        saldovacanze=saldovacanze_attuale-ore_assenza
+        dipendente_record.values['saldovacanze'] = saldovacanze
+        dipendente_record.save()
+
     
 
     #TODO
@@ -2764,7 +2775,8 @@ def get_record_attachments(request):
             file=attachment['file']
             type=attachment['type']
             note=attachment['note']
-            attachment_list.append({'recordid':recordid,'file':file,'type':type, 'note':note})
+            filename=note=attachment['filename']
+            attachment_list.append({'recordid':recordid,'file':file,'type':type, 'note':note, 'filename':filename})
             
         response={ "attachments": attachment_list}
         print(response)
