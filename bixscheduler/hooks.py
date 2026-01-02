dab3a3a3d2a57a780f183c520a03c52bc6527955
Sara Bressan <sara.bressan@swissbix.ch>
2026-01-02 15:15:45 +0100

    Update risolto errore di ricerca schedule

--- bixscheduler/hooks.py
+++ bixscheduler/hooks.py
@@ -37,7 +37,7 @@ def on_task_success(task):
         pass
 
     try:
-        schedule = Schedule.objects.filter(task=task.id).first()  # ottieni il Schedule
+        schedule = Schedule.objects.filter(func=task.func).first()  # ottieni il Schedule
         if not schedule:
             return None
 
