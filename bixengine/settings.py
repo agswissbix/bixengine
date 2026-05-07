439a63993abfa99e9030db9b37505992b1ff6d40
riccardodeiuri <riccardo.deiuri@swissbix.ch>
2026-05-07 17:01:09 +0200

    scheduler django admin

--- bixengine/settings.py
+++ bixengine/settings.py
@@ -231,8 +231,11 @@ Q_CLUSTER = {
     'timeout': 900, # The number of seconds a worker is allowed to spend on a task before it’s terminated.
     'queue_limit': 50, # This does not limit the amount of tasks that can be queued on the broker
     'bulk': 1, # Sets the number of messages each cluster tries to get from the broker per call
-    'orm': 'default',
+    'orm': 'default', # TODO usare redis
     'catch_up': False, # The default behavior for schedules that didn’t run while a cluster was down, is to play catch up and execute all the missed time slots until things are back on schedule.
+    'compress': True, # NOVITÀ: Risparmia spazio sul database
+    'recycle': 100,   # NOVITÀ SALVA-RAM: Riavvia il worker ogni 100 task completati
+    'max_attempts': 3,
     'HOOKS': ['bixscheduler.hooks.on_task_success'],
 }
 
--- bixengine/urls.py
+++ bixengine/urls.py
@@ -6,6 +6,7 @@ from bixengine.core.admin import custom_admin_site
 
 urlpatterns = [
     path('admin/', custom_admin_site.urls), 
+    path('django_admin/', admin.site.urls), 
     path('login/', custom_admin_site.login, name='admin_login'),
     path('logout/', custom_admin_site.logout, name='admin_logout'),    
     path('auth/login/', login_view, name='login'),
